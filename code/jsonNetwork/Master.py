7f250360b78d3768a9f9b4e4a73c447eaa5ffc7a
trevstanhope <tpstanhope@gmail.com>
2013-03-20 04:16:22 -0400

    send/receive for Master and Slave

--- code/jsonNetwork/Master.py
+++ code/jsonNetwork/Master.py
@@ -7,22 +7,37 @@ from socket import *
 import json
 
 ## Constants
-HOST_OUT = '' # we are the host
-PORT_OUT = 30000
+HOST_OUT = '' # master is host
+HOST_IN = 'localhost' # address of slave
+PORT_OUT = 30000 # master port
+PORT_IN = 30001 # slave port
 ADDR_OUT = (HOST_OUT,PORT_OUT) # ADDR tuple
+ADDR_IN = (HOST_IN,PORT_IN) # ADDR tuple
 BUFSIZE = 4096 # reasonable size
 
 ## Bind Socket
-serv = socket(AF_INET,SOCK_STREAM) # create a new socket object (serv)
-serv.bind((ADDR_OUT)) # bind socket to address with new tuple
-serv.listen(5) # maximum number of queued connections
+socket_out = socket(AF_INET,SOCK_STREAM) # create a new socket object (serv)
+socket_out.bind((ADDR_OUT)) # bind socket to address with new tuple
+socket_out.listen(5) # maximum number of queued connections
 
 while 1:
+	# SEND
     print 'listening...'
-    conn,addr = serv.accept() # accept the connection
+    conn,addr = socket_out.accept() # accept the connection
     print '...connected!'
     HEADER = "TYPE"
     PAYLOAD = "DATA"
     msg = json.dumps({'HEADER':HEADER, 'PAYLOAD':PAYLOAD}) # dump to string
     conn.send(msg)
     conn.close()
+    
+    # RECEIVE
+    socket_in = socket(AF_INET,SOCK_STREAM) # tmp socket
+    socket_in.connect((ADDR_IN))
+    msg = socket_in.recv(BUFSIZE) # get message as string
+    jmsg = json.loads(msg) # re-encode message as json
+    HEADER = jmsg["HEADER"] # parse header
+    PAYLOAD = jmsg["PAYLOAD"] # parse payload
+    print HEADER
+    print PAYLOAD
+    socket_in.close()
