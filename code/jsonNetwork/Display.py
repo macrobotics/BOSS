99b4646ba4ace92125d18392495ec5dc7358a67a
Trevor Stanhope <trevor@t510.(none)>
2013-04-21 00:23:05 -0400

    proto GUI

--- code/jsonNetwork/Display.py
+++ code/jsonNetwork/Display.py
@@ -6,11 +6,44 @@
 import pygtk
 pygtk.require('2.0')
 import gtk
+from socket import *
+import json
+
+## Global
+HOST_OUT = '' # master is host
+HOST_IN = 'localhost' # address of slave
+PORT_OUT = 30000 # master port
+PORT_IN = 30001 # slave port
+ADDR_OUT = (HOST_OUT,PORT_OUT) # ADDR tuple
+ADDR_IN = (HOST_IN,PORT_IN) # ADDR tuple
+BUFSIZE = 4096 # reasonable size
 
 class Display:
     def callback(self, widget, data):
         print "%s was pressed" % data
-        
+        socket_out = socket(AF_INET,SOCK_STREAM) # create a new socket object (serv)
+        socket_out.bind((ADDR_OUT)) # bind socket to address with new tuple
+        socket_out.listen(5) # maximum number of queued connections
+        while data == "START":
+            print "listening..." # need method to break loop on STOP
+            conn,addr = socket_out.accept() # accept the connection
+            print '...connected!'
+            HEADER = "TYPE"
+            PAYLOAD = "DATA"
+            msg = json.dumps({'HEADER':HEADER, 'PAYLOAD':PAYLOAD}) # dump to string
+            conn.send(msg)
+            conn.close()
+    
+            # RECEIVE
+            socket_in = socket(AF_INET,SOCK_STREAM) # tmp socket
+            socket_in.connect((ADDR_IN))
+            msg = socket_in.recv(BUFSIZE) # get message as string
+            jmsg = json.loads(msg) # re-encode message as json
+            HEADER = jmsg["HEADER"] # parse header
+            PAYLOAD = jmsg["PAYLOAD"] # parse payload
+            print HEADER
+            print PAYLOAD
+            socket_in.close()
     def delete_event(self, widget, event, data=None):
         gtk.main_quit()
         return False
